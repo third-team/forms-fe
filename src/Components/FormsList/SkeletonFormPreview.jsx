73656cfa0bd12eaa06a4935c6dbbc19335517c3d
Oleksandr <alex.dragonfox@gmail.com>
2021-07-07 15:07:34 +0300

    Fixed FormsList Skeleton

--- src/Components/FormsList/SkeletonFormPreview.jsx
+++ src/Components/FormsList/SkeletonFormPreview.jsx
@@ -3,19 +3,19 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 function SkeletonFormPreview() {
 	return (
 		<SkeletonTheme color='#343434' highlightColor='#4d4d4d'>
-			<div className='form-name'>
+			<div className='forms-list__form-name'>
 				<h1>
 					<Skeleton width={250} duration={0.75} />
 				</h1>
-				<div className='button-container'>
+				<div className='forms-list__buttons-wrapper'>
 					<button type='button' style={{ padding: 'initial' }} className='button button-success'>
 						<div style={{ height: '100%', width: '100%' }}>
-							<Skeleton height={'95%'} width={'95%'} duration={0.75} />
+							<Skeleton height={'100%'} width={'100%'} style={{ display: 'flex' }} duration={1} />
 						</div>
 					</button>
 					<button type='button' style={{ padding: 'initial' }} className='button button-danger'>
 						<div style={{ height: '100%', width: '100%' }}>
-							<Skeleton height={'95%'} width={'95%'} duration={0.75} />
+							<Skeleton height={'100%'} width={'100%'} style={{ display: 'flex' }} duration={1} />
 						</div>
 					</button>
 				</div>
