24d1d9b025c33617387b9efd5b9e3b8a656e1051
Oleksandr <alex.dragonfox@gmail.com>
2021-07-07 17:17:53 +0300

    Updated proxy set method

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import axios from 'axios';
+
 import './styles/utils.scss';
 import './index.scss';
 
@@ -12,6 +14,8 @@ import appReducer from './redux/reducers';
 import App from './App';
 
 const store = createStore(appReducer);
+
+axios.defaults.baseURL = 'https://third-team-forms.herokuapp.com';
 
 ReactDOM.render(
 	<React.StrictMode>
